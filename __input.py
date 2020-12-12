6aa13f1cc414b6400b906f55c876452ca8268b5a
ElectroMagneticInterference <73551650+ElectroMagneticInterference@users.noreply.github.com>
2020-12-12 13:35:58 -0800

    Some Initial parts I have created.

---
+++ __input.py
@@ -0,0 +1,8 @@
+import drivetrain, brain
+
+drivetrain.Start(-900, -900)
+drivetrain.speed(110, 110)
+drivetrain.Move_to([[900, -900], [123, -677], [0,0]])
+drivetrain.end()
+
+input()
---
+++ brain.py
@@ -0,0 +1,21 @@
+def clear():
+    print("    brain.clear()")
+def cl():
+    print("    brain.clear()")
+def p(t):
+    print("    brain.print(\"" + str(t) + "\")")
+def newline():
+    print("    brain.new_line()")
+def n():
+    print("    brain.new_line()")
+def color(c):
+    print("    brain.set_print_color(" + str(c.upper()) + ")")
+def c(c):
+    print("    brain.set_print_color(" + str(c.upper()) + ")")
+
+
+
+
+
+
+
